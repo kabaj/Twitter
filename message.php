af10d2a72c918e488800b8d3d50a1ef182749786
Filip Kabaj <filip.kabaj@gmail.com>
2016-02-14 21:58:33 +0100

    Gotowe w 95%

--- message.php
+++ message.php
@@ -16,28 +16,3 @@ $msgToShow = Message::getMessageById($id);
 echo("{$msgToShow->getDataWyslania()}");
 echo("<p>{$msgToShow->getText()}</p>");
 
-
-/*
-$messageToShow = Message::getMessageById($messageId);
-if($messageToShow->getSendId() == $_SESSION['userId'] || $messageToShow->getReceiveId() == $_SESSION['userId']) {
-    $sendingUser = User::GetUserById($messageToShow->getSendId());
-    $receiveingUser = User::GetUserById($messageToShow->getReceiveId());
-//var_dump($receiveingUser);
-    if ($_SESSION['userId'] == $messageToShow->getReceiveId()) {
-        $messageToShow->changeStatus();
-    }
-    echo("<strong> Odbiorca:</strong> {$receiveingUser->getName()} <br />
-    <strong>Nadawca:</strong> {$sendingUser->getName()} <br />
-    <strong>Tresc: </strong> {$messageToShow->getMessageText()} <br />
-    {$messageToShow->getMessageDate()} <br />");
-    if ($_SESSION['userId'] != $messageToShow->getReceiveId()) {  //wysłanie kolejnej wiadomości do tego samego użytkownika
-        echo("<a href='sendwiadomosc.php?id={$receiveingUser->getId()}'>Wyślij kolejną wiadomość do {$receiveingUser->getName()}</a> <br />");
-    }
-    if ($_SESSION['userId'] != $messageToShow->getSendId()) {  //wysłanie odpowiedzi na wiadomość użytkownika
-        echo("<a href='sendwiadomosc.php?id={$sendingUser->getId()}'>Odpowiedz użytkownikowi {$sendingUser->getName()}</a> <br />");
-    }
-    echo("<a href='allMessage.php.php'>Wróć do wszystkich wiadomości</a>
-    ");
-
-
-*/
